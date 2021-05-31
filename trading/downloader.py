f00a3d55284c5f0fdc35da83581a0a318c5e85c3
mhconradt <mhconradt@protonmail.com>
2021-05-31 16:44:50 -0500

    initialize

---
+++ trading/downloader.py
@@ -0,0 +1,38 @@
+from datetime import datetime, timedelta
+import typing as t
+
+import cbpro
+import pyarrow as pa
+import pyarrow.parquet as pq
+
+from trading.candlesticks import CoinbaseCandleCollector
+
+
+class MarketDownloader:
+    def __init__(self, root_path: str,
+                 fs: pa.fs.FileSystem = pa.fs.LocalFileSystem()):
+        self.root_path = root_path
+        self.fs = fs
+
+    def download(self, start: datetime, end: datetime,
+                 markets: t.Optional[t.List[str]] = None) -> None:
+        frequencies = {'15m': timedelta(minutes=15),
+                       '5m': timedelta(minutes=5)}
+        if markets is None:
+            products = cbpro.PublicClient().get_products()
+            markets = [description['id'] for description in products]
+        collector = CoinbaseCandleCollector()
+        for freq_name, frequency in frequencies.items():
+            df = collector.get_markets(markets, start, end, frequency)
+            df = df.reset_index()
+            df['freq'] = freq_name
+            df['date'] = df['time'].dt.strftime('%F')
+            df = df.set_index(['freq', 'market', 'time'])
+            tbl = pa.Table.from_pandas(df)
+            pq.write_to_dataset(tbl, self.root_path, filesystem=self.fs,
+                                partition_cols=['date', 'freq'])
+
+
+if __name__ == '__main__':
+    downloader = MarketDownloader('../.market_data')
+    downloader.download(datetime(2021, 5, 1), datetime.now())
---
+++ trading/indicators.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def _leading_momentum(df: pd.DataFrame, lead: int, span: int) -> pd.Series:
+    return (df.close.shift(lead) / df.close.shift(lead + span)) - 1.
+
+
+def leading_momentum(df: pd.DataFrame, lead: int = 1,
+                     span: int = 1) -> pd.Series:
+    return df.groupby(level=['freq', 'market'], group_keys=False).apply(
+        _leading_momentum,
+        lead=lead,
+        span=span)
+
+
+def _momentum(df: pd.DataFrame, span: int) -> pd.Series:
+    return (df.close / df.close.shift(span)) - 1.
+
+
+def momentum(df: pd.DataFrame, span: int = 1) -> pd.Series:
+    return df.groupby(level=['freq', 'market'], group_keys=False).apply(
+        _momentum, span=span)
+
+
+def autocorrelation(df: pd.DataFrame, lag: int = 1):
+    return df.open.groupby(level=['freq', 'market'], group_keys=False).apply(
+        lambda s: s.autocorr(lag))
+
+
+def moon_score(df: pd.DataFrame) -> pd.Series:
+    short_term = df.loc[['15m', '5m']]
+    mom = momentum(short_term)
+    mom1 = leading_momentum(short_term)
+    mom_15m, mom1_15m = mom.loc['15m'], mom1.loc['15m']
+    moon_15m = (mom_15m > mom1_15m) * (mom_15m - mom_15m)
+    mom_5m, mom1_5m = mom.loc['5m'], mom1.loc['5m']
+    moon_5m = (mom_5m > mom1_5m) * (mom_5m - mom1_5m)
+    # candle for t-15min is most recent at t
+    adj = moon_15m.unstack(0).shift(freq='15min').resample('5min').ffill(2)
+    return moon_5m.unstack(0) + adj
+
+
+def main() -> None:
+    df = pd.read_parquet('../.market_data')
+    scores = moon_score(df)
+    scores[['ADA-USD', 'BTC-USD', 'ETH-USD']].plot()
+    plt.show()
+
+
+__all__ = ['momentum', 'leading_momentum', 'moon_score']
+
+
+if __name__ == '__main__':
+    main()
