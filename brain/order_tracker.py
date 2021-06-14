281031d0dff5830abe5bf2f7f349b252eacd217f
mhconradt <mhconradt@protonmail.com>
2021-06-14 15:38:37 -0500

    Fix "What if we find an un-watched order?" in SyncCoinbaseOrderTracker
    Signed-off-by: mhconradt <mhconradt@protonmail.com>

--- brain/order_tracker.py
+++ brain/order_tracker.py
@@ -21,37 +21,51 @@ class OrderTracker(ABC):
         ...
 
     @abstractmethod
-    def un_track(self, order_id: str) -> None:
+    def untrack(self, order_id: str) -> None:
         ...
 
 
 class SyncCoinbaseOrderTracker(OrderTracker):
-    def barrier_snapshot(self) -> t.Tuple[str, dict]:
-        timestamp = self.client.get_time()['iso']
-        return timestamp, self.snapshot()
-
     def __init__(self, client: AuthenticatedClient,
                  watchlist: t.Optional[t.List[str]] = None):
         self.client = client
         self.watchlist = [] if watchlist is None else watchlist
 
+    @property
+    def active_order_count(self) -> int:
+        return len(self.watchlist)
+
     def track(self, order_id: str) -> None:
         self.watchlist.append(order_id)
 
+    def barrier_snapshot(self) -> t.Tuple[str, dict]:
+        timestamp = self.client.get_time()['iso']
+        return timestamp, self.snapshot()
+
     def snapshot(self) -> dict:
         index = len(self.watchlist) - 1
+        watched_items = set(self.watchlist)
         snapshot = {}
         for order in self.client.get_orders(status='all'):
-            for i in range(index, 0, -1):
+            print(order['id'])
+            for i in range(index, -1, -1):
                 watch = self.watchlist[i]
                 order_id = order['id']
-                index -= 1
+                print(f"{order_id} -> {watch}")
                 if watch == order_id:
+                    index -= 1
                     snapshot[order_id] = order
                     break
+                elif order_id not in watched_items:
+                    # ignore this order
+                    break
                 else:
+                    # watched item was canceled
+                    index -= 1
                     self.un_track(watch)
+            else:
+                break
         return snapshot
 
-    def un_track(self, order_id: str) -> None:
+    def untrack(self, order_id: str) -> None:
         self.watchlist.remove(order_id)
