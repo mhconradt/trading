3efeb064dbbf7e702ccf8d9fc34dd52abd8d8a59
mhconradt <mhconradt@protonmail.com>
2021-08-16 22:18:03 -0500

    be less greedy
    Signed-off-by: mhconradt <mhconradt@protonmail.com>

--- brain/portfolio_manager.py
+++ brain/portfolio_manager.py
@@ -166,7 +166,7 @@ class PortfolioManager:
         self.quote_account_id = [account['id'] for account in accounts if
                                  account['currency'] == self.quote][0]
         self.tracker = order_tracker
-        self.pop_limit = Decimal('0.33')
+        self.pop_limit = Decimal('0.25')
         self.pov_limit = Decimal('1')
 
         self.counter = PositionCounter()
