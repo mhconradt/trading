56e1940bbd53d4aa982bb812b8813d42b3daf892
mhconradt <mhconradt@protonmail.com>
2021-08-26 15:36:20 -0500

    propagate risk management update to helm
    Signed-off-by: mhconradt <mhconradt@protonmail.com>

--- settings/portfolio.py
+++ settings/portfolio.py
@@ -6,5 +6,4 @@ env = Env()
 
 EXCHANGE = env.str('EXCHANGE', 'coinbasepro')
 STOP_LOSS = Decimal(env.str('STOP_LOSS', '0.99'))
-TAKE_PROFIT = Decimal(env.str('TAKE_PROFIT', '2'))
 QUOTE = env.str('QUOTE', 'USD')
