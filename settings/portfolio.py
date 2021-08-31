cf3a797c2a28d50867858e55bc028eb32b4c7f4e
mhconradt <mhconradt@protonmail.com>
2021-08-31 14:32:59 -0500

    extract stop loss cooldown to settings
    Signed-off-by: mhconradt <mhconradt@protonmail.com>

--- settings/portfolio.py
+++ settings/portfolio.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from decimal import Decimal
 
 from environs import Env
@@ -8,3 +9,5 @@ EXCHANGE = env.str('EXCHANGE', 'coinbasepro')
 STOP_LOSS = Decimal(env.str('STOP_LOSS', '0.99'))
 QUOTE = env.str('QUOTE', 'USD')
 MIN_TICK_TIME = env.float('MIN_TICK_TIME', 0.)
+STOP_LOSS_COOLDOWN = env.timedelta('COOLDOWN_SECONDS', timedelta(hours=1),
+                                   precision='seconds')
