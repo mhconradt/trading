869313f94d9484eb2c82575bb056692d2bc0485a
mhconradt <mhconradt@protonmail.com>
2021-09-14 15:26:58 -0500

    make code nan safe
    Signed-off-by: mhconradt <mhconradt@protonmail.com>

--- backtest/__init__.py
+++ backtest/__init__.py
@@ -1,6 +1,13 @@
+import sys
+import time
+
+import numba
 import numpy as np
 
+FLOAT_INFO_EPSILON = sys.float_info.epsilon
 
+
+@numba.njit
 def simulate(fiat: float, buy_fraction: np.array, sell_fraction: np.array,
              price: np.array, fee: float, buy_expiration: int = 30,
              sell_expiration: int = 30) -> np.array:
@@ -11,8 +18,8 @@ def simulate(fiat: float, buy_fraction: np.array, sell_fraction: np.array,
     :param sell_fraction: [t, m] = fraction of balance[m] to sell at t
     :param price: [t, m] = price of m at t
     :param fee: fee paid to exchange as fraction of spending amount
-    :param buy_expiration:
-    :param sell_expiration:
+    :param buy_expiration: the number of periods after which the buy expires
+    :param sell_expiration: the number of periods after which the sell expires
     :return: the final market value of the portfolio
     """
     m = buy_fraction.shape[1]
@@ -21,52 +28,66 @@ def simulate(fiat: float, buy_fraction: np.array, sell_fraction: np.array,
     sell_sizes = np.zeros((sell_expiration, m))
     sell_prices = np.zeros((sell_expiration, m))
     balance = np.zeros(m)
+    order_price = np.full(m, FLOAT_INFO_EPSILON)
     for t in range(buy_fraction.shape[0]):
+        order_price = np.where(~np.isnan(price[t]), price[t], order_price)
         # holds -> balance
         buy_fills = buy_prices > price[t]  # filled if market moves below price
-        fiat_fill_total = (buy_sizes[buy_fills] * buy_prices[buy_fills]).sum()
+        fiat_fill_total = (buy_sizes * buy_fills * buy_prices).sum()
         fiat -= fiat_fill_total * fee
-        filled_size = np.where(buy_fills, buy_sizes, 0.).sum(axis=0)
+        filled_size = (buy_sizes * buy_fills).sum(axis=0)
         balance += filled_size
-        buy_sizes[buy_fills] = 0.
+        buy_sizes *= ~buy_fills
         # holds -> fiat
         sell_fills = sell_prices < price[t]
-        proceeds = (sell_sizes[sell_fills] * sell_prices[sell_fills]).sum()
+        proceeds = (sell_sizes * sell_fills * sell_prices).sum()
         fiat += proceeds * (1 - fee)
-        sell_sizes[sell_fills] = 0.
+        sell_sizes *= ~sell_fills
         # expiration
         balance += sell_sizes[t % sell_expiration]
         fiat += buy_sizes[t % buy_expiration] @ buy_prices[t % buy_expiration]
         # buys -> holds
         buy_quote_amount = fiat * buy_fraction[t]
         fiat -= buy_quote_amount.sum()
-        buy_base_amount = buy_quote_amount / price[t]
+        buy_base_amount = buy_quote_amount / order_price
         buy_sizes[t % buy_expiration] = buy_base_amount
-        buy_prices[t % buy_expiration] = price[t]
+        buy_prices[t % buy_expiration] = order_price
         # sells -> holds
         sell_base_amount = balance * sell_fraction[t]
         balance -= sell_base_amount
         sell_sizes[t % sell_expiration] = sell_base_amount
-        sell_prices[t % sell_expiration] = price[t]
+        sell_prices[t % sell_expiration] = order_price
         continue
     return fiat
 
 
 def generate_price(t: int, mean: float, std: float) -> np.array:
+    """
+    Simulate log-normal price movements
+    """
     moves = np.random.randn(t) * std + mean
     return np.exp(moves).cumprod()
 
 
 def main() -> None:
     np.random.seed(42)
-    m = 3
+    m = 64
     t = 86400
     fee = 0.0008
+    expiration = 5
     buy_fraction = (1 - (1 - np.random.rand(t, m)) ** (1 / 300)) / m
     sell_fraction = 1 - (1 - np.random.rand(t, m)) ** (1 / 300)
     price = np.hstack([np.expand_dims(generate_price(t, 0.00001, 0.0001), -1)
-                       for _ in range(3)])
-    print(simulate(100_000., buy_fraction, sell_fraction, price, fee))
+                       for _ in range(m)])
+    drop = np.random.permutation(np.arange(price.size).reshape(*price.shape))
+    price[drop % 11 == 0] = np.nan
+    # numba warmup
+    start = time.time()
+    for i in range(7):
+        print(simulate(100_000., buy_fraction, sell_fraction, price, fee,
+                       expiration,
+                       expiration))
+    print(f"{time.time() - start:.2f}s")
 
 
 if __name__ == '__main__':
